78c2c1f6325a6f9fe89dd22731e38fc82a7025da
kessie870 <kn_caldwell@yahoo.com>
2018-01-28 10:13:01 -0600

    Add files via upload

---
+++ Liri node app/keys.js
@@ -0,0 +1,17 @@
+var spotify = new Spotify(keys.spotify);
+  var client = new Twitter(keys.twitter);
+
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: 'k87nRmswrBXTaTeemJa7EqWPE',
+  consumer_secret: 'TryLlvIlA6mhOXfggBM4XHaKGxd1t0GpgkhR8nrnJUFVAQTAW7q',
+  access_token_key: '957063554984706048-8rLMb8a6xaX0yZjdBSF55Rfx2PTqupU',
+  access_token_secret:'5chZBWi2ddXDzRFEltnsuPwZDfqXJHK95zb16Ano7885R'
+};
+
+exports.spotify = {
+  id: '6fc5927835b9423d81822f5ad122d735',
+  secret: 'b1788bec499844b7b2fe8d2af37329bf',
+};
+
